d8a15570a2d1407c8db75f9f98b6164fc552e425
mangollc <89349388+mangollc@users.noreply.github.com>
2024-12-14 12:09:43 -0500

    Update components/layout/header/nav/desktop/nav-content-item.tsx [BoltSync]

--- components/layout/header/nav/desktop/nav-content-item.tsx
+++ components/layout/header/nav/desktop/nav-content-item.tsx
@@ -14,19 +14,17 @@ export function NavContentItem({ name, href, description, className }: NavConten
       <Link
         href={href}
         className={cn(
-          "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
-          "hover:bg-muted hover:text-foreground focus:bg-muted focus:text-foreground",
-          "dark:hover:bg-muted/50 dark:hover:text-foreground dark:focus:bg-muted/50 dark:focus:text-foreground",
+          "nav-menu-link",
           className
         )}
       >
-        <div className="text-sm font-medium leading-none">{name}</div>
+        <div className="text-sm font-medium">{name}</div>
         {description && (
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <p className="line-clamp-2 text-sm text-muted-foreground">
             {description}
           </p>
         )}
       </Link>
     </li>
-  );
+  )
 }
