7d2cdd8a2f0f0ee9de589f0a9aa5302a2f60c504
mangollc <89349388+mangollc@users.noreply.github.com>
2024-12-14 12:09:42 -0500

    Update components/layout/header/nav/desktop/nav-content.tsx [BoltSync]

--- components/layout/header/nav/desktop/nav-content.tsx
+++ components/layout/header/nav/desktop/nav-content.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { NavigationMenuContent } from "@/components/ui/navigation-menu"
 import { NavContentItem } from "./nav-content-item"
 import type { NavLink } from "@/lib/constants/navigation"
@@ -8,12 +10,12 @@ interface NavContentProps {
 
 export function NavContent({ items }: NavContentProps) {
   return (
-    <NavigationMenuContent>
+    <NavigationMenuContent className="nav-menu-content">
       <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
         {items.map((item) => (
           <NavContentItem key={item.href} {...item} />
         ))}
       </ul>
     </NavigationMenuContent>
-  );
+  )
 }
