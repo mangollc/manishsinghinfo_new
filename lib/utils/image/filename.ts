bf9f16b5c11b7afcc03a270d1960a6618987143b
mangollc <89349388+mangollc@users.noreply.github.com>
2024-12-24 16:31:13 -0500

    Update lib/utils/image/filename.ts [BoltSync]

---
+++ lib/utils/image/filename.ts
@@ -0,0 +1,6 @@
+export function generateUniqueFilename(originalName: string): string {
+  const timestamp = Date.now()
+  const random = Math.random().toString(36).substring(2, 15)
+  const extension = originalName.split('.').pop()
+  return `${timestamp}-${random}.${extension}`
+}
